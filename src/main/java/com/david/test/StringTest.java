9a54de0fc0a442df7f2809f54ea3b6f95cd9017d
David <986945193@qq.com>
2020-03-25 16:52:23 +0800

    update test

---
+++ src/main/java/com/david/test/StringTest.java
@@ -0,0 +1,14 @@
+package com.david.test;
+
+/**
+ * @author David
+ * @date 2020/3/25
+ * @time 16:41
+ */
+public class StringTest {
+    public static void main(String[] args) {
+//        String[] arrays = "1。2".split("。");
+        String[] arrays = "1.2".split("\\.");
+        System.out.println(arrays.length);
+    }
+}
